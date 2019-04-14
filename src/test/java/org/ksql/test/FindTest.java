1b978d13a5b60f0ee31a6616fec67549a1007b4f
kyssion <1409915687@qq.com>
2019-04-14 20:18:48 +0800

    update

--- src/test/java/org/ksql/test/FindTest.java
+++ src/test/java/org/ksql/test/FindTest.java
@@ -4,8 +4,10 @@ import org.junit.ClassRule;
 import org.junit.experimental.theories.DataPoint;
 import org.ksql.script.annotation.Mapper;
 
+import java.awt.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
+import java.util.Map;
 
 public class FindTest {
     public static void main(String[] args) throws NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
@@ -14,9 +16,9 @@ public class FindTest {
         item.getAnnotations();//获取所有的注解(包括继承的注解,但是并不包括重复注解)
 
         Method method =
-                item.getMethod("method",new Class[]{Object.class});
+                item.getMethod("method", new Class[]{Object.class});
         item.getMethods();
-        item.getDeclaredMethod("method",new Class[]{Object.class});
+        item.getDeclaredMethod("method", new Class[]{Object.class});
         item.getDeclaredMethods();
         item.getEnclosingMethod();//返回这个封闭类的所有方法
 
@@ -29,19 +31,42 @@ public class FindTest {
         method.getGenericReturnType();//获取变量的type类型
         method.getReturnType();//获取变量的返回值class
 
-        method.invoke(new Object(),new Object[]{});
+        method.invoke(new Object(), new Object[]{});
+
+        method.getParameterAnnotations();
+        method.getDeclaredAnnotations();
+        method.getDeclaredAnnotation(Mapper.class);
+        method.getDeclaredAnnotationsByType(Mapper.class);
+
+        method.getDefaultValue();
+        method.getName();
+        method.getModifiers();
+        method.isBridge();
+        method.isDefault();
+        method.isSynthetic();
+        method.isVarArgs();
+
+        item.getConstructor(String.class);
+        item.getConstructor();
+        item.getConstructors();
+        item.getDeclaredConstructor(String.class);
+        item.getDeclaredConstructors();
+
+        Constructor constructor = item.getConstructor();
+
     }
 }
 
-class FindChild extends Find<String>{
+class FindChild extends Find<String> {
 
 }
 
-class Find<T>{
+class Find<T> {
     @ClassRule
     @DataPoint
     T test;
-    public <S> T method(T name){
+
+    public <S> T method(T name) {
         return this.test;
     }
 }
