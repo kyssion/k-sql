506b1f7593392e7173a7b5657e2d4c9fd7e98bd9
kyssion <1409915687@qq.com>
2019-06-10 17:57:48 +0800

    udpate

--- src/main/java/org/ksql/script/builder/MapperClassFactoryBuilder.java
+++ src/main/java/org/ksql/script/builder/MapperClassFactoryBuilder.java
@@ -24,7 +24,6 @@ public class MapperClassFactoryBuilder {
         for (Class<?> mapperClass : mapperSet) {
             if(mapperClass.isInterface()) {
                 mapperClassFactory.create(mapperClass);
-
             }
         }
         return mapperClassFactory;
